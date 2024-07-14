2c402fb0ecd93811cfbc811c61757599361bbcc8
Suren Rodrigo <surenr@99x.io>
2024-07-14 17:59:48 +0530

    modified sample data to chage organization and project names

--- flowyzer-schemas/V015__flowyzer_app_db_sample_data.sql
+++ flowyzer-schemas/V015__flowyzer_app_db_sample_data.sql
@@ -1,23 +1,23 @@
 -- Inserting into flowyzer_organizations
 INSERT INTO "flowyzer_organizations" ("id", "name", "isActive", "createdAt", "updatedAt")
 VALUES
-('d902f6a0-5315-49ea-9ebb-65c3df507de3', 'BUS-AS-Norway', false, '2024-06-18T05:32:09.254Z', '2024-06-18T05:32:09.254Z');
+('d902f6a0-5315-49ea-9ebb-65c3df507de3', 'SAMPLE-AS-Norway', false, '2024-06-18T05:32:09.254Z', '2024-06-18T05:32:09.254Z');
 
 -- Inserting into flowyzer_teams
 INSERT INTO "flowyzer_teams" ("id", "name", "organization_id", "createdAt", "updatedAt")
 VALUES
-('f63597b2-1ead-4800-b5e5-5c0dce21750e', 'BUS', 'd902f6a0-5315-49ea-9ebb-65c3df507de3', '2024-06-18T05:32:09.254Z', '2024-06-18T05:32:09.254Z');
+('f63597b2-1ead-4800-b5e5-5c0dce21750e', 'SAMPLE', 'd902f6a0-5315-49ea-9ebb-65c3df507de3', '2024-06-18T05:32:09.254Z', '2024-06-18T05:32:09.254Z');
 
 -- Inserting into flowyzer_dashboards
 INSERT INTO "flowyzer_dashboards" ("id", "name", "team_id")
 VALUES
-('85d0c497-3eb5-4369-ae98-6331d6249bad', 'BUS Dashboard', 'f63597b2-1ead-4800-b5e5-5c0dce21750e');
+('85d0c497-3eb5-4369-ae98-6331d6249bad', 'SAMPLE Dashboard', 'f63597b2-1ead-4800-b5e5-5c0dce21750e');
 
 -- Inserting into flowyzer_connections
 INSERT INTO "flowyzer_connections" ("id", "name", "sourceProvider", "sourceId", "destinationSourceId", "destinationId", "sourceCatalogId", "connectionId", "dashboard_id")
 VALUES
-('5918e9fc-d958-4f27-9344-eda48612cefb', 'BUS-AS-Norway-BUS-AzureRepo<->Faros Destination', '{"apiUrl":"https://dev.azure.com", "apiVersion":"7.0", "pageSize":100, "maxRetries":3, "graphApiUrl":"https://vssps.dev.azure.com", "graphVersion":"7.1-preview.1", "requestTimeout":60000, "rejectUnauthorized":true, "kind":"AzureRepo", "branchName":".*", "name":"BUS-AS-Norway-BUS-AzureRepo", "organization":"BUS-AS-Norway", "accessToken":"Sample_token", "projects":["BUS"], "refreshToken":"sample_token", "lastRenewed":"2024-06-27T07:16:02.148Z"}', '751f0de4-6446-429f-8145-d00e0238144d', '76018b0c-1db4-483b-b1d7-f54c0ceb7533', 'c6ae16ef-7fd1-4472-873b-1e6498913e7a', '11c8bb04-9138-41bb-b817-fdb53bcfc236', 'b8fc3678-4d50-4a09-93dc-e7a9983a4adc', '85d0c497-3eb5-4369-ae98-6331d6249bad'),
-('5220dcbf-3d39-4803-9467-00e7f23ee9db', 'BUS-AS-Norway-BUS-AzureWorkItems<->Faros Destination', '{"apiVersion":"7.0", "graphVersion":"7.1-preview.1", "requestTimeout":60000, "kind":"AzureWorkItems", "name":"BUS-AS-Norway-BUS-AzureWorkItems", "organization":"BUS-AS-Norway", "accessToken":"sample_token", "projects":["BUS"], "refreshToken":"sample_token"}', 'f7d18b3d-7462-4b4b-b470-7623ce3199fb', '76018b0c-1db4-483b-b1d7-f54c0ceb7533', 'c6ae16ef-7fd1-4472-873b-1e6498913e7a', '2ca33027-59ff-442c-bebd-436eb7258f3d', 'ee3b84ab-f3f0-43d3-a0a2-6117df2e330e', '85d0c497-3eb5-4369-ae98-6331d6249bad');
+('5918e9fc-d958-4f27-9344-eda48612cefb', 'SAMPLE-AS-Norway-SAMPLE-AzureRepo<->Faros Destination', '{"apiUrl":"https://dev.azure.com", "apiVersion":"7.0", "pageSize":100, "maxRetries":3, "graphApiUrl":"https://vssps.dev.azure.com", "graphVersion":"7.1-preview.1", "requestTimeout":60000, "rejectUnauthorized":true, "kind":"AzureRepo", "branchName":".*", "name":"SAMPLE-AS-Norway-SAMPLE-AzureRepo", "organization":"SAMPLE-AS-Norway", "accessToken":"Sample_token", "projects":["SAMPLE"], "refreshToken":"sample_token", "lastRenewed":"2024-06-27T07:16:02.148Z"}', '751f0de4-6446-429f-8145-d00e0238144d', '76018b0c-1db4-483b-b1d7-f54c0ceb7533', 'c6ae16ef-7fd1-4472-873b-1e6498913e7a', '11c8bb04-9138-41bb-b817-fdb53bcfc236', 'b8fc3678-4d50-4a09-93dc-e7a9983a4adc', '85d0c497-3eb5-4369-ae98-6331d6249bad'),
+('5220dcbf-3d39-4803-9467-00e7f23ee9db', 'SAMPLE-AS-Norway-SAMPLE-AzureWorkItems<->Faros Destination', '{"apiVersion":"7.0", "graphVersion":"7.1-preview.1", "requestTimeout":60000, "kind":"AzureWorkItems", "name":"SAMPLE-AS-Norway-SAMPLE-AzureWorkItems", "organization":"SAMPLE-AS-Norway", "accessToken":"sample_token", "projects":["SAMPLE"], "refreshToken":"sample_token"}', 'f7d18b3d-7462-4b4b-b470-7623ce3199fb', '76018b0c-1db4-483b-b1d7-f54c0ceb7533', 'c6ae16ef-7fd1-4472-873b-1e6498913e7a', '2ca33027-59ff-442c-bebd-436eb7258f3d', 'ee3b84ab-f3f0-43d3-a0a2-6117df2e330e', '85d0c497-3eb5-4369-ae98-6331d6249bad');
 
 -- Inserting into flowyzer_syncs
 INSERT INTO "flowyzer_syncs" ("id", "jobStarted", "jobLastUpdated", "syncDateTime", "status", "connection_id")
